dd2ce3863712af63dd6c5e3d0ac344974a202a37
dasuco <79526322+dasuco@users.noreply.github.com>
2021-05-10 10:33:08 +0200

    Initial commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Sucias Colomer, David on 10/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
